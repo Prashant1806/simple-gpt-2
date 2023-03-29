b8493af2414747dab1a2bd431dae38c4e327f251
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2023-03-30 00:47:58 +0530

    Update GPT-2.py

--- GPT-2.py
+++ GPT-2.py
@@ -1,4 +1,5 @@
 # Import necessary libraries
+import tkinter as tk
 from transformers import pipeline, set_seed
 
 # Set seed for reproducibility
@@ -12,13 +13,38 @@ def generate_response(prompt, max_length=100):
     response = generator(prompt, max_length=max_length, do_sample=True, temperature=0.7)[0]['generated_text']
     return response.strip()
 
-# Set up the chatbot loop
-while True:
-    # Get user input
-    user_input = input("You: ")
+# Define function to handle user input
+def send_message(event=None):
+    # Get user input from entry widget
+    user_input = user_input_entry.get()
+    user_input_entry.delete(0, tk.END)
 
-    # Generate a response
+    # Generate a chatbot response
     chatbot_response = generate_response(user_input)
 
-    # Print the chatbot response
-    print("Chatbot: " + chatbot_response)
+    # Add user input and chatbot response to the chatbox
+    chatbox.config(state=tk.NORMAL)
+    chatbox.insert(tk.END, "You: " + user_input + "\n\n")
+    chatbox.insert(tk.END, "Chatbot: " + chatbot_response + "\n\n")
+    chatbox.config(state=tk.DISABLED)
+    chatbox.see(tk.END)
+
+# Set up GUI
+root = tk.Tk()
+root.title("Generative Transformer Chatbot")
+
+# Add chatbox
+chatbox = tk.Text(root, height=20, width=50, state=tk.DISABLED)
+chatbox.pack(padx=10, pady=10)
+
+# Add user input entry widget
+user_input_entry = tk.Entry(root, width=50)
+user_input_entry.pack(padx=10, pady=10)
+user_input_entry.bind("<Return>", send_message)
+
+# Add send button
+send_button = tk.Button(root, text="Send", command=send_message)
+send_button.pack(padx=10, pady=10)
+
+# Start GUI main loop
+root.mainloop()
